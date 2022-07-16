ea31607d26495480d9db27ffb1f6942336deeb17
Laurence Cadag <laurencecads1@gmail.com>
2022-07-16 15:20:25 +0800

    Day 7

---
+++ Day 07 - Events/events.js
@@ -0,0 +1,47 @@
+const butts = document.querySelector('.butts');
+const coolBtn = document.querySelector('.cool');
+const buyBtns = document.querySelectorAll('button.buy');
+const photoEl = document.querySelector(".photo");
+
+function handleClick() {
+  console.log('IT GOT CLICKED!!!!!!!!');
+}
+
+butts.addEventListener('click', handleClick);
+coolBtn.addEventListener('click', handleClick);
+
+butts.removeEventListener('click', handleClick);
+
+
+// buyBtns.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     console.log(`clicked ${button.textContent}`);
+    
+//   })
+// })
+
+const handleByButtonClick = (event) => {
+  const button = event.target;
+  console.log("You clicked a button");
+  // event.stopPropagation();
+  console.log(event.target);
+  console.log(event.currentTarget);
+  console.log(event.target === event.currentTarget);
+}
+
+buyBtns.forEach(function(button){
+  button.addEventListener('click', handleByButtonClick);
+})
+
+window.addEventListener("click", function(event) {
+  console.log("you clicked the window");
+  console.log(event.target);
+  console.log(event.type);
+  console.log(event.bubbles);
+},
+{ capture: true }
+);
+
+photoEl.addEventListener("mousemove", function(e) {
+  console.log(e.currentTarget);
+});
